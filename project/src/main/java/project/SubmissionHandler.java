54f84bb83dbfd1e81234f64a15f980cf64f8391c
Ethanael7 <123254382+Ethanael7@users.noreply.github.com>
2024-11-16 16:43:56 -0400

    progress1

--- project/src/main/java/project/SubmissionHandler.java
+++ project/src/main/java/project/SubmissionHandler.java
@@ -8,6 +8,9 @@ public class SubmissionHandler {
     @SuppressWarnings("unused")
     private ScoreCalculator scoreCalculator;
     private ReportGenerator reportGenerator;
+    int runs = 10;
+    int compiles = 5;
+    int passes = 10;
 
     public SubmissionHandler() {
         this.scoreCalculator = new ScoreCalculator();
@@ -40,9 +43,11 @@ public class SubmissionHandler {
 
         System.out.println("Total ChatBotPlatform Marks: " + totalPlatformMarks);
 
-        int totalMark = totalChatBotMarks + totalPlatformMarks;
+        int totalMark = (totalChatBotMarks + totalPlatformMarks) + runs + compiles + passes;
 
         System.out.println("Total Marks: " + totalMark);
+
+
     }
 
 
