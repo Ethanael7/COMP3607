3f1719fbc5c3160ac01c2cae03d35b4491b37431
Shaynelle Edwards <shaynelle.e8@gmail.com>
2024-11-18 13:31:12 -0400

    Implementation update 2

--- output_dir/comp3607a2/comp3607a2/src/main/java/comp3607a2/CreateQuiz.java
+++
@@ -1,8 +0,0 @@
-package comp3607a2;
-public interface CreateQuiz {
-    public SpellIterator createAlphabeticalIterator();
-    public SpellIterator createCorrectIterator();
-    public SpellIterator createDurationIterator();
-    public SpellIterator createFluencyIterator();
-    public SpellIterator createErrorCorrectionIterator();
-}
--- output_dir/comp3607a2/comp3607a2/src/main/java/comp3607a2/SpellIterator.java
+++
@@ -1,6 +0,0 @@
-package comp3607a2;
-public interface SpellIterator {
-    public boolean hasNext();
-    public SpellingAttempt next();
-    public int getLength();
-}
